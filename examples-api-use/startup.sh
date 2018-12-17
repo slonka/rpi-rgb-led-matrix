a3a98f3b2a51d868050af9b9ff8f8d427227ca9f
Krzysztof Slonka <krzysztof.slonka@allegro.pl>
2018-12-17 15:03:06 +0100

    Check for named pipe existance 3

--- examples-api-use/startup.sh
+++ examples-api-use/startup.sh
@@ -6,4 +6,4 @@ fi
 
 cat <>/tmp/ledpipe &
 
-sudo ./examples-api-use/text-example -y 2 --led-rows=16 --led-cols=32 --led-chain=1 --led-parallel=2 --led-multiplexing=10 -f ./fonts/5x7.bdf --led-inverse -b 1 < /tmp/ledpipe
+sudo ./examples-api-use/text-example -y 2 --led-rows=16 --led-cols=32 --led-chain=2 --led-parallel=1 --led-multiplexing=10 -f ./fonts/5x7.bdf --led-inverse -b 1 < /tmp/ledpipe
