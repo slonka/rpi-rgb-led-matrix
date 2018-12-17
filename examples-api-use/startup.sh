e11160e6750c630348f098336a63d819a017c489
Krzysztof Slonka <krzysztof.slonka@allegro.pl>
2018-12-17 15:01:55 +0100

    Check for named pipe existance 3

--- examples-api-use/startup.sh
+++ examples-api-use/startup.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-if [ ! -f /tmp/ledpipe ]; then
+if [ ! -p /tmp/ledpipe ]; then
     mkfifo /tmp/ledpipe
 fi
 
 cat <>/tmp/ledpipe &
 
-sudo ./examples-api-use/text-example -y 2 --led-rows=16 --led-cols=32 --led-chain=1 --led-parallel=1 --led-multiplexing=10 -f ./fonts/5x7.bdf --led-inverse -b 1 < /tmp/ledpipe
+sudo ./examples-api-use/text-example -y 2 --led-rows=16 --led-cols=32 --led-chain=1 --led-parallel=2 --led-multiplexing=10 -f ./fonts/5x7.bdf --led-inverse -b 1 < /tmp/ledpipe
